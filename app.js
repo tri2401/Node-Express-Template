271eef4cabde201c5e85a68082ca0699838315ca
tri2401 <tri2401na@gmail.com>
2022-09-20 17:46:29 -0500

    routes added using module.exports

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require('express');
+const meme = require('./meme');
 const app = express();
 
 app.get('/', function(req, res){
-    res.send('hello world');
+    res.send({'message':'hello world'});
 });
 
 app.listen(5000, function (){
     console.log('listening at 5000');
 });
+
+app.use('/deeznuts', meme.deeznuts);
+app.use('/21', meme.wutsnineplusten);
 
 module.exports = app;
---
+++ meme.js
@@ -0,0 +1,12 @@
+
+
+function deeznuts(req, res, next){
+    res.send('HAAAAA Gotem');
+}
+
+function wutsnineplusten(req, res, next){
+    res.send('you stupid');
+}
+
+module.exports.deeznuts = deeznuts;
+module.exports.wutsnineplusten = wutsnineplusten;
